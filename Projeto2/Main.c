6c17ca4c70457a4584c975b9995d6642c153a74f
Flavio Lins da Mota Nascimento <flmn@uhwc22.windows.cin.ufpe.br>
2019-03-27 16:30:41 -0300

    project2 ajusted

--- Projeto2/Main.c
+++ Projeto2/Main.c
@@ -1,85 +1,77 @@
 #include <REG51F.H>
 
-sbit P2_0 = P2^0;
-sbit P2_1 = P2^1;
+sbit P2_0 = P2^0;  //Primeiro bit da Porta2
+sbit P2_1 = P2^1;  //Segundo bit da Porta2
 
-int transicao01(int a, int b){
-	if(a & !b) {
-		//val = P2_1;
-		return 1;
+void checkBit1();  //Acompanha mudanças no Segundo bit da Porta2
+void checkBit0();  //Acompanha mudanças no Primeiro bit da Porta2
+
+void main() {
+
+	while(1) {
+		checkBit1();
+		checkBit0();
 	}
-	else
-		return 0;
 }
 
-void checkbit0(){
 
-	if(P2_0 == 1)
-	{
-		//P1L = P0L;
-		P1 = (P1 & 0xF0)|(P0 & 0x0F);
-		
+void checkBit1() {
+	static unsigned char estado = 0;  //Considera o estado inicial na primeira inicialização
+	static unsigned int counter, countermult = 0; 
+	switch (estado) {
+		case 0:
+			if (P2_1 == 0) {
+				estado = 1;
+			}
+			break;
+		case 1:
+			if (P2_1 == 1) {
+				P1 = (P1 & 0x0F) | (P0 & 0xF0);
+				counter = 0;
+				estado = 2;
+			}
+			break;	
+		case 2:
+			if (counter++ == 65000) { //Loop de 1 segundo
+				countermult+=1;
+				if (countermult==5) {
+					P1 = P1 & 0x0F;
+					countermult = 0;
+					estado = 0;
+				}
+				counter=0;
+			}
+			break;
 	}
-	else
-	{
-		//P1L = 0;
-		P1 = (P1 & 0xF0);
-		
-	}
-	
 }
-		
-void checkbit1(){
-	if(P2_1 == 1)
-	{
-		//P1H = P0H;
-		P1 = (P1 & 0x0F)|(P0 & 0xF0);
-		
-	}
-	else
-	{
-		//P1H = 0;
-		P1 = (P1 & 0x0F);
-		
-	}
-	
-}
-	
-void main()
-{
-	int val = P2_1;
-	int estado = 0;
-	while(1)
-	{
-		
+
+void checkBit0() {
+	static unsigned char estado = 0;  //Considera o estado inicial na primeira inicialização
+    static unsigned int counter, countermult = 0;
 		switch (estado) {
-			case 0:
-				if (transicao01(P2_1, val)) {
-					P1 = 0x01;
-					val = P2_1;
-					estado = 1;
-				}
-				
-				break;
-			case 1:
-				if (transicao01(P2_1, val)) {
-					P1 = 0x00;
-					val = P2_1;
+		case 0:
+			if (P2_0 == 0) {
+				estado = 1;
+			}
+			break;
+		case 1:
+			if (P2_0 == 1) {
+				P1 = (P1 & 0xF0) | (P0 & 0x0F);
+				counter = 0;
+				estado = 2;
+			}
+			break;
+		case 2:
+			
+			if (counter++ == 65000) { //Loop de 1 segundo
+				countermult+=1;
+				if (countermult==5) {
+					P1 = P1 & 0xF0;
+					countermult = 0;
 					estado = 0;
 				}
-				break;	
-			case 2:
-				val = P2_1;
-				if (transicao01(P2_1, val)) {
-					P1 = 0xF0;
-					estado = 0;
-				}
-				break;
-		}
-		
-		//checkbit0();
-		//checkbit1();
-		
+				counter=0;
+			}
+			break;
 	}
-	
 }
